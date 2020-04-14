1dff0a44b060c13c98ce96be35929dc515ed50c2
Git Sadiq <abdulaisadiq@live.com>
2020-04-14 13:38:24 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('broly cheese burger')
+INSERT INTO burgers
+    (name)
+VALUES
+    ('raditz burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('cheeseburger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('picollo burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('veggieta burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('goku burger')
